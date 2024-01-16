28f926452ab7b4be8338907dd372e4836cdc70d1
Andi <76712811+andimendunia@users.noreply.github.com>
2024-01-16 07:57:34 +0700

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,4 +1,5 @@
+title ASSEMBLY CONVEYOR MONITORING - MM
+pnputil /enable-device "USB\VID_1A86&PID_7523\5&89f2c13&0&3"
 cd /d C:\acm-py
-start python main.py
-nircmd cmdwait 5000 win hide ititle python
-exit
+nircmd cmdwait 3000 win hide ititle "CONVEYOR MONITORING"
+python main.py
